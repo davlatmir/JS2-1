b232344a0d808e6e9ffd76e33d2b260226ab69ed
davlatmir <davlatmirshamsherov@gmail.com>
2022-09-24 00:04:56 +0500

    +1

--- script.js
+++ script.js
@@ -13,15 +13,16 @@ const products = [
     { id: 10, title: 'Биг Фишбургер', price: 200, img: "https://shef.tj/product-api/picture/42195?type=picture_original" },
 ];
 
+/*
 let isBasketPopuped = false
 
 function popupBasket() {
     isBasketPopuped = !isBasketPopuped
     document.getElementById('basket-dialog').style.display = isBasketPopuped ? 'flex' : ''
 }
-
+*/
 const renderProduct = (item) =>
-             `<div class="product-item">
+             `<div class="product-item data-id="1">
                 <img src="${item.img}" alt="image" class="product-img">
                 <h3 class="product-title">${item.title}</h3>
                 <p class="product-price">${item.price} руб</p>
@@ -35,10 +36,95 @@ const renderProducts = list => {
 renderProducts(products);
 
 
-/* 
-1. Добавьте стили для верхнего меню, товара, списка товаров и кнопки вызова корзины.
+class CartItem {
+    constructor(name, img, price){
+        this.name = name
+        this.img = img
+        this.price = price
+        this.quantity = 1
 
-2. Добавьте значения по умолчанию для аргументов функции. Как можно упростить или сократить запись функций?
+    }
+}
 
-3. *Сейчас после каждого товара на странице выводится запятая. Из-за чего это происходит? Как это исправить?
-*/
+class LocalCart {
+    static key = 'carItems'
+
+    static getLocalCartItems() {
+        let cartMap = new Map()
+        const cart = localStorage.getItem(key)
+        if(cart === null || cart.length === 0) return cartMap
+       return new Map(Object.entries(JSON.parse(cart)))
+    }
+
+
+     static addItemToLocalCart(id, item){
+        let cart = LocalCart.getLocalCartItems()
+        if (cart.has(id)) {
+        let item = cart.get(id) 
+            mapItem.quantity +=1
+            cart.set(id, mapItem)
+     } else 
+     cart.set(id, item)
+     localStorage.setItem(this.key, JSON.stringify(Object.fromEntries(cart)))
+     updateCartUI()
+    }
+        static removeItemFromCart(id){
+    let cart = LocalCart.getLocalCartItems()
+    if(cart.has(id)){
+        let mapItem = cart.get(id)
+        if(mapItem.quantity>1)
+       {
+        mapItem.quantity -=1
+        cart.set(id, mapItem)
+       }
+       else
+       cart.delete(id)
+    } 
+    if (cart.length===0)
+    localStorage.clear()
+    else
+    localStorage.setItem(LocalCart.key,  JSON.stringify(Object.fromEntries(cart)))
+       updateCartUI()
+    }
+}
+
+const cartIcon = document.querySelector('.cart-button')
+const wholeCartWindow = document.querySelector('.whole-cart-window')
+wholeCartWindow.inWindow = 0
+
+const addToCartBtns = document.querySelectorAll('.by-btn')
+addToCartBtns.forEach( (btn)=>{
+    btn.addEventListener('click', addItemFunction)
+}  );
+
+function addItemFunction(e){
+    const id = e.target.parentElement.parentElement.parentElement.getAttribute("data-id")
+    const desc = e.target.parentElement.children[0].textContent
+    let price = e.target.parentElement.children[1].textContent
+    price = price.replace("Price: $", '')
+ console.log(price)
+}
+
+cartIcon.addEventListener('mouseover', ()=> {
+    if (wholeCartWindow.classList.contains('hide'))
+    wholeCartWindow.classList.remove('hide')
+})
+
+cartIcon.addEventListener('mouseleave', ()=> {
+   // if (wholeCartWindow.classList.contains('hide'))
+   setTimeout( () => {
+    if (wholeCartWindow.inWindow === 0) {
+        wholeCartWindow.classList.add('hide')
+    }
+   } ,500 )
+   
+})
+
+wholeCartWindow.addEventListener('mouseover', () => {
+    wholeCartWindow.inWindow = 1    
+})
+
+wholeCartWindow.addEventListener('mouseleave', () => {
+    wholeCartWindow.inWindow = 0    
+     wholeCartWindow.classList.add('hide')
+})
