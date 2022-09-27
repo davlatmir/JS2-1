297bbd001c49f4a08019138d48deea2df8f36976
davlatmir <davlatmirshamsherov@gmail.com>
2022-09-27 20:50:42 +0500

    done some basket

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var basket = [],
+priceTotal = 0
+
 const products = [
     { id: 1, title: 'Бургер', price: 150, img: "https://chitirchicken.tj/image/cache/catalog/products/bifburger-550x550.jpg" },
     { id: 2, title: 'Чизбургер', price: 80, img: "https://tastyandpoint.com/wp-content/uploads/2021/05/dvojnoj-chizburger-465x465.png" },
@@ -13,118 +16,138 @@ const products = [
     { id: 10, title: 'Биг Фишбургер', price: 200, img: "https://shef.tj/product-api/picture/42195?type=picture_original" },
 ];
 
-/*
-let isBasketPopuped = false
+class Product {
+    constructor(id, title, price, img, containerClass) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.img = img;
+        this.quantity = 1;
+        this.dom = null;
+        this.containerClass = containerClass
+        this.render()
+    }
 
+    getTemplate() {
+        return `
+         <div class="whole-cart-item ">
+             <div>
+                 <img class="mini-cardImg" src="${this.img}">
+             </div>
+             <div style="padding: 0px 12px;">
+                 <div class="itemName">${this.title}</div>
+                 <div class="quantity">Quantity: ${this.quantity}</div>
+                 <div class="price">Price: ${this.price}</div>
+                 <button  class="removeButton">remove</button>
+             </div>
+         </div>
+         `;
+    }
+
+    render() {
+        var container = document.querySelector(this.containerClass);
+        if (container) {
+
+            container.insertAdjacentHTML("beforeend", this.getTemplate());
+            this.dom = container.lastElementChild
+            if (this.dom) {
+                this.dom.self = this;
+                var removeButton = this.dom.querySelector('.removeButton')
+                if (removeButton) removeButton.onclick = () => this.decrement()
+            }
+
+        }
+    }
+
+    update() {
+        if (this.dom) {
+            var price = this.dom.querySelector(".price"),
+                quantity = this.dom.querySelector(".quantity");
+            if (price && quantity) {
+                price.innerText = `Price: ${this.price} (${this.price * this.quantity})`;
+                quantity.innerText = `Quantity: ${this.quantity}`;
+                console.log(priceTotal)
+            }
+        }
+    }
+
+    increment() {
+        this.quantity += 1;
+        priceTotal += this.price
+        this.update();
+        console.log(this)
+    }
+
+    decrement() {
+        this.quantity -= 1
+        priceTotal -= this.price
+
+        if (this.quantity > 1) {
+        } else {
+            this.dom.remove()
+            var productIndex = basket.findIndex(arrItem => arrItem.id == this.id);
+            if (productIndex > -1) {
+                basket.splice(productIndex, 1)
+            }
+        }
+        this.update();
+    }
+}
+
+
+
+let isBasketPopuped = false
 function popupBasket() {
     isBasketPopuped = !isBasketPopuped
     document.getElementById('basket-dialog').style.display = isBasketPopuped ? 'flex' : ''
 }
-*/
+
+
+
+function addToBasket(id) {
+    var product = products.find(element => element.id == id)
+    if (product) {
+
+        var productIndex = basket.findIndex(element => element.id == id)
+        if (productIndex > -1) {
+            basket[productIndex].increment()
+        } else {
+            basket.push(new Product(product.id, product.title, product.price, product.img, '.basket-dialog-content'))
+        }
+    }
+
+}
+
+
+
+
+
+
+// priceTotal-а нишон тияд и количество товаров в корзине
+
+
+function updateUI () {
+    dom.innerText = priceTotal
+}
+
+
+
+
+
+
+
+
+
+
 const renderProduct = (item) =>
              `<div class="product-item data-id="1">
                 <img src="${item.img}" alt="image" class="product-img">
                 <h3 class="product-title">${item.title}</h3>
                 <p class="product-price">${item.price} руб</p>
-                <button class="by-btn">В корзину</button>
+                <button class="by-btn" onclick="addToBasket(${item.id})">В корзину</button>
             </div>`;
 
 const renderProducts = list => {
     document.querySelector('.mainContainer').insertAdjacentHTML('beforeend', list.map(item => renderProduct(item)).join(''));
 };
-
 renderProducts(products);
-
-
-class CartItem {
-    constructor(name, img, price){
-        this.name = name
-        this.img = img
-        this.price = price
-        this.quantity = 1
-
-    }
-}
-
-class LocalCart {
-    static key = 'carItems'
-
-    static getLocalCartItems() {
-        let cartMap = new Map()
-        const cart = localStorage.getItem(key)
-        if(cart === null || cart.length === 0) return cartMap
-       return new Map(Object.entries(JSON.parse(cart)))
-    }
-
-
-     static addItemToLocalCart(id, item){
-        let cart = LocalCart.getLocalCartItems()
-        if (cart.has(id)) {
-        let item = cart.get(id) 
-            mapItem.quantity +=1
-            cart.set(id, mapItem)
-     } else 
-     cart.set(id, item)
-     localStorage.setItem(this.key, JSON.stringify(Object.fromEntries(cart)))
-     updateCartUI()
-    }
-        static removeItemFromCart(id){
-    let cart = LocalCart.getLocalCartItems()
-    if(cart.has(id)){
-        let mapItem = cart.get(id)
-        if(mapItem.quantity>1)
-       {
-        mapItem.quantity -=1
-        cart.set(id, mapItem)
-       }
-       else
-       cart.delete(id)
-    } 
-    if (cart.length===0)
-    localStorage.clear()
-    else
-    localStorage.setItem(LocalCart.key,  JSON.stringify(Object.fromEntries(cart)))
-       updateCartUI()
-    }
-}
-
-const cartIcon = document.querySelector('.cart-button')
-const wholeCartWindow = document.querySelector('.whole-cart-window')
-wholeCartWindow.inWindow = 0
-
-const addToCartBtns = document.querySelectorAll('.by-btn')
-addToCartBtns.forEach( (btn)=>{
-    btn.addEventListener('click', addItemFunction)
-}  );
-
-function addItemFunction(e){
-    const id = e.target.parentElement.parentElement.parentElement.getAttribute("data-id")
-    const desc = e.target.parentElement.children[0].textContent
-    let price = e.target.parentElement.children[1].textContent
-    price = price.replace("Price: $", '')
- console.log(price)
-}
-
-cartIcon.addEventListener('mouseover', ()=> {
-    if (wholeCartWindow.classList.contains('hide'))
-    wholeCartWindow.classList.remove('hide')
-})
-
-cartIcon.addEventListener('mouseleave', ()=> {
-   // if (wholeCartWindow.classList.contains('hide'))
-   setTimeout( () => {
-    if (wholeCartWindow.inWindow === 0) {
-        wholeCartWindow.classList.add('hide')
-    }
-   } ,500 )
-   
-})
-
-wholeCartWindow.addEventListener('mouseover', () => {
-    wholeCartWindow.inWindow = 1    
-})
-
-wholeCartWindow.addEventListener('mouseleave', () => {
-    wholeCartWindow.inWindow = 0    
-     wholeCartWindow.classList.add('hide')
-})
